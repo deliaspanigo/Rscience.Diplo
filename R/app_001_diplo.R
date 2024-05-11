797c3567fa4423b6117d029d0470b950d64d3f78
deliaspanigo <david.elias949@gmail.com>
2024-05-11 19:52:22 -0300

    init 8

--- R/app_001_diplo.R
+++ R/app_001_diplo.R
@@ -216,13 +216,13 @@ server <- function(input, output) {
     #rmarkdown::render("report.Rmd", output_file = "report.html")
     #aver <- system.file("vignettes", "report.Rmd")
     #rmarkdown::render("report.Rmd", output_file = "report.html")
-    htmltools::includeMarkdown(base::system.file("vignettes", "report.Rmd", package = "Rscience.Diplo"))
+    #htmltools::includeMarkdown(base::system.file("vignettes", "report.Rmd", package = "Rscience.Diplo"))
     # Incluir el HTML en la interfaz de usuario
     output$rmd_output <- shiny::renderUI({
 
-      rmarkdown::render(base::system.file("inst/extdata/", "report.Rmd", package = "Rscience.Diplo"), output_format = "html_document")
+      rmarkdown::render(base::system.file("extdata", "report.Rmd", package = "Rscience.Diplo"), output_format = "html_document")
 
-      html_file_path <- base::system.file("inst/extdata/", "report.html", package = "Rscience.Diplo")
+      html_file_path <- base::system.file("extdata", "report.html", package = "Rscience.Diplo")
 
       html_content <- base::readLines(html_file_path)
 
